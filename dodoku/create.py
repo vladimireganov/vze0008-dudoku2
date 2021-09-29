2401551928bb518af4ff0049ba7b0ad8cc12a2a6
vladimireganov <38359772+vladimireganov@users.noreply.github.com>
2021-09-29 05:33:08 -0500

    update

--- dodoku/create.py
+++ dodoku/create.py
@@ -13,12 +13,11 @@ def _create(parms):
     elif level == 3:
         grid = [0,0,0,0,-6,0,0,0,0,0,0,0,-4,0,-9,0,0,0,0,0,-9,-7,0,-5,-1,0,0,0,-5,-2,0,-7,0,-8,-9,0,-9,0,0,-5,0,-2,0,0,-4,0,-8,-3,0,-4,0,-7,-2,0,0,0,-1,-2,0,-8,0,0,0,0,0,0,-6,0,-4,0,0,0,0,0,0,0,-5,0,0,0,0,0,-9,-8,-2,0,0,0,-4,-3,0,0,-7,0,0,0,0,0,0]                                
     else:
-        print("error")
+        return {'error': 'Invalid level input'}
     
     data = ""
     for i in grid:
         data+= str(grid[i])
-    
     integrety = hashlib.sha256(data.encode('utf-8')).hexdigest()
     result = {'grid':grid, 'integrity': integrety ,'status': 'ok'}
     return result
